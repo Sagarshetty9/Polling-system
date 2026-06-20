82b0b7ae9c3e9a05bd8a7dd5ab63b46606664951
Sagar Shetty <shettysagar619@gmail.com>
2026-06-20 11:34:04 +0530

    socket error fixes

--- backend/controller/createPoll.js
+++ backend/controller/createPoll.js
@@ -1,4 +1,4 @@
-import { getIO } from '../config/socket.js';
+import { initIO } from '../config/socket.js';
 import Poll from '../model/pollingSchema.js'; // Ensure correct imports matching your schemas
 import Team from '../model/teamSchema.js';
 
